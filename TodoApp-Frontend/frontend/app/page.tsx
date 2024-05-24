0a21d723b1dde2e59300e02d8ae885fbb0365f34
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-05-25 03:58:44 +0500

    Update page.tsx

--- TodoApp-Frontend/frontend/app/page.tsx
+++ TodoApp-Frontend/frontend/app/page.tsx
@@ -1,21 +1,3 @@
-// import { getAllTodos } from "./api";
-// import TodoList from "./components/TodoList";
-// import AddTask from './components/AddTask';
-// export default async function Home() {
-//   const tasks = await getAllTodos();
-
-//   return (
-//     <main className='max-w-4xl mx-auto mt-4'>
-//       <div className='text-center my-5 flex flex-col gap-4'>
-//         <h1 className='text-4xl font-bold mb-8 mt-6'> Todo List App</h1>
-//         <AddTask />
-//       </div>
-//       <TodoList tasks={tasks} />
-//     </main>
-//   );
-// }
-
-
 // Import necessary functions and components
 import { getAllTodos } from "./api";
 import TodoList from "./components/TodoList";
